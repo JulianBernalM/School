8c01804f3162b0d40a7382cec620d6ec8deb028d
Julian Bernal <Julian-1705@hotmail.com>
2022-09-14 16:13:56 -0500

    A class student in file mock-student is created and import this in file component.ts

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../mock-student';
 
 @Component({
   selector: 'app-students',
@@ -6,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.scss']
 })
 export class StudentsComponent implements OnInit {
-
-  student = 'Julian Bernal';
 
   constructor() { }
 
