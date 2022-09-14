35a2e20114df9a658c619f6ee5c312b53e40d03f
Julian Bernal <Julian-1705@hotmail.com>
2022-09-14 13:14:07 -0500

    A new student is added

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentsComponent implements OnInit {
 
+  student = 'Julian Bernal';
+
   constructor() { }
 
   ngOnInit(): void {
