e50bb7c17a276de1f12aee726b87550a52e1c332
Julian Bernal <Julian-1705@hotmail.com>
2022-09-29 15:23:37 -0500

    Add new component named edit-student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent
+    StudentsComponent,
+    EditStudentComponent
   ],
   imports: [
     BrowserModule,
