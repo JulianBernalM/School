67ca1adbf273d840a227fef53667df6d57857d31
Julian Bernal <Julian-1705@hotmail.com>
2022-10-06 11:25:25 -0500

    Add method that return simulated students

--- src/app/school.service.ts
+++ src/app/school.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Student } from './class-student';
+import { STUDENTS } from './mock-students';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchoolService {
+
+  public getStudents(): Student[] {
+    return STUDENTS;
+  }
 
   constructor() { }
 }
